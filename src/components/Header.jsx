1e2aba4b2859351b565e7655dc458334d32f521d
zejd1 <87140363+zejd1@users.noreply.github.com>
2025-02-15 17:39:06 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header(){
     <>
         <header className='flex items-center justify-between gap-4 p-4'>
           <a href="/">
-            <h1 className="font-medium">Understand<span className='text-blue-400 bold'>IT</span></h1>
+            <h1 className="font-medium">Understand<span className='text-blue-400 bold'>It</span></h1>
           </a>
           <a href="/" className="flex items-center gap-2 specialBtn px-4 py-2 rounded-lg text-blue-400">
             <p>New</p> 
